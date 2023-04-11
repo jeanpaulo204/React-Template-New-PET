bb8cd98781f82bd87aaa03d362c7685a52a54126
Jean Paulo <jean.paulo@edu.itajai.sc.gov.br>
2023-04-11 16:43:54 -0300

    new Project

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { Header } from './Menu/components/header';
+import { Inicio } from './Menu/inicio/template';
+
+function App() {
+  return (
+<div>
+<div className="App">
+<Header/>
+</div>
+<Inicio/>
+</div>
+
+  );
+}
+
+export default App;
---
+++ app/src/Menu/inicio/template.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Slider } from './components/imageCard';
+
+
+
+export const Inicio = () => {
+
+
+ 
+
+return (
+
+<div >
+  <Slider/>
+</div>
+
+
+);
+
+
+}
