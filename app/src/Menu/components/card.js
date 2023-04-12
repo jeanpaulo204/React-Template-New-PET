73267949fce5ee4678f1ec44b0633c26dbe53a8d
Jean Paulo <jean.paulo@edu.itajai.sc.gov.br>
2023-04-12 16:18:23 -0300

    mobile first

--- app/src/Menu/components/card.js
+++ app/src/Menu/components/card.js
@@ -7,6 +7,8 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Icon from '@mdi/react';
 import { mdiCommentProcessing } from '@mdi/js';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
 
 export const Newcard = ({title, texto, btn , comentario , image}) => {
 
@@ -15,8 +17,18 @@ return (
     
 <Card sx={{ maxWidth: 550 }}>
 <CardActions>
-        <Button variant="outlined" size="small">{btn}</Button>
-        <Button variant="outlined" color="success"  size="small">{comentario} '<Icon path={mdiCommentProcessing} size={1} /></Button>
+        
+   
+  <Box sx={{ flexGrow: 1 }}>
+  <Grid container spacing={4} columns={10}>
+  <Grid  xl={5} sm={5} md={5}>
+  <Button variant="outlined" size="small">{btn}</Button>
+  </Grid>
+  <Grid  xl={5} sm={3} md={5}>
+  <Button variant="outlined" color="success"  size="small">{comentario}<Icon path={mdiCommentProcessing} size={1} /></Button>
+  </Grid>
+  </Grid>
+  </Box>
         
 </CardActions>
       <CardMedia
